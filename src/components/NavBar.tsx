3a20ce18495bd497170589381e0b9256a8329926
Ayanlola Bayo Timileyin <timmyshow96@gmail.com>
2023-08-13 18:01:01 +0100

    Build the color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import d818svg from '../assets/D818 - SVG.svg';
 import d818png from '../assets/D818 - PNG.png';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={d818svg} boxSize="60px" />
-      <Text>D818 Restaurant</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };

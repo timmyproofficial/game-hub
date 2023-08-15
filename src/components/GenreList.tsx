c965c08d8dd8e07ee717552aeecbfaf183bd09c3
Ayanlola Bayo Timileyin <timmyshow96@gmail.com>
2023-08-15 21:16:29 +0100

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,25 @@
 import useGenres from '../hooks/useGenre';
+import { List, ListItem, Image, Text, HStack } from '@chakra-ui/react';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+              alt={genre.name}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
